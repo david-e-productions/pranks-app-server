54cba0bac7496eadb57a73f183a5afb29d943a00
Davide Loprewsti <davidelopresti0129@gmail.com>
2023-01-30 11:48:24 +0000

    add nodmailer route

--- routes/nodemailer.routes.js
+++ routes/nodemailer.routes.js
@@ -1,31 +1,31 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-router.post("/prank", async (req, res) => {
-  const { email, time, place, name } = req.body;
+// router.post("/prank", async (req, res) => {
+//   const { email, time, place, name } = req.body;
 
-  const transporter = nodemailer.createTransport({
-    service: "hotmail",
-    auth: {
-      user: "yourprankapp@outlook.it",
-      pass: "Z123456a!!!",
-    },
-  });
+//   const transporter = nodemailer.createTransport({
+//     service: "hotmail",
+//     auth: {
+//       user: "yourprankapp@outlook.it",
+//       pass: "Z123456a!!!",
+//     },
+//   });
 
-  const options = {
-    from: "yourprankapp@outlook.it",
-    to: { email },
-    subject: "🍕🍕🍕Free pizza!",
-    text: `🍕 Dear ${name} is youre lucky day! 
-    🍕 You have been selected to come to oure free all you can eat pizza party! 
-    🍕 We are looking forward to see you at ${time} in ${place}! 
-    🍕 See you there tanti abbracci from youre pizza place preferito.🍕 `,
-  };
+//   const options = {
+//     from: "yourprankapp@outlook.it",
+//     to: { email },
+//     subject: "🍕🍕🍕Free pizza!",
+//     text: `🍕 Dear ${name} is youre lucky day!
+//     🍕 You have been selected to come to oure free all you can eat pizza party!
+//     🍕 We are looking forward to see you at ${time} in ${place}!
+//     🍕 See you there tanti abbracci from youre pizza place preferito.🍕 `,
+//   };
 
-  transporter.sendMail(options, (err, info) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log("sent: " + info.response);
-  });
-});
+//   transporter.sendMail(options, (err, info) => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log("sent: " + info.response);
+//   });
+// });
