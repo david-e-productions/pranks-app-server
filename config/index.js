efc54ae32e52cd9306910be637dbeeba1b4d0ff2
Davide Loprewsti <davidelopresti0129@gmail.com>
2023-01-31 15:53:33 +0000

    changes for deploy

--- config/index.js
+++ config/index.js
@@ -24,8 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: ["http://localhost:3000", process.env.Origin],
-      credentials: true,
+      origin: ["http://localhost:3000", process.env.ORIGIN],
     })
   );
 
